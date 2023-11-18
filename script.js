606e845e2f059865285871fdaf5983662b0b795b
Fenar Khan <fenarkhan7@gmail.com>
2023-11-18 12:22:13 +0530

    djfksdkjf

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ var embeddedVideo = document.getElementById('embeddedVideo');
 embeddedVideo.addEventListener('touchstart', function(event) {
     event.preventDefault();
 });
+
+window.addEventListener('beforeunload', function(event) {
+    // Prevent any new windows from opening
+    window.open = function() {
+        // Optionally, you can log the attempt to open a popup
+        console.log("Popup blocked");
+        return null;
+    };
+});

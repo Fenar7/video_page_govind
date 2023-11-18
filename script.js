d97db34e4c42b6f4994c3aa0c38713c6dce8f2f5
Fenar Khan <fenarkhan7@gmail.com>
2023-11-18 13:12:48 +0530

    fdgdfg

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-var embeddedVideo = document.getElementById('embeddedVideo');
-
-// Prevent default behavior on touchstart event
-embeddedVideo.addEventListener('touchstart', function(event) {
+window.oncontextmenu = function(event) {
     event.preventDefault();
-});
-
-window.addEventListener('beforeunload', function(event) {
-    // Prevent any new windows from opening
-    window.open = function() {
-        // Optionally, you can log the attempt to open a popup
-        console.log("Popup blocked");
-        return null;
-    };
-});
+    event.stopPropagation();
+    return false;
+};
